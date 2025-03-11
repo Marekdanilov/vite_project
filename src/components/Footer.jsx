6b97e0ed1f029fb86fa53f9f75567f952bf1a981
Marek Danilov <danmarekb@gmail.com>
2025-03-11 23:18:40 +0900

    update Footer and Header with Desing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,83 @@ import { Link } from 'react-router-dom'
 
 function Footer() {
     return (
-        <div className='bg-gray-800 text-gray-50 bottom-0 h-[200px]'>
-            <div className='text-xl  font-semibold'>Footer:</div>
+        <div className='flex justify-center bg-[#454545] text-white bottom-0 h-[200px] px-6 py-2'>
+            {/* <div className='text-xl  font-semibold'>Footer:</div>
             <div className='text-blue-500 space-x-2'>
                 <Link to='/'>Home</Link>
                 <Link to='/blog'>Blog</Link>
                 <Link to='/About'>About</Link>
+            </div> */}
+            <div className='flex flex-row gap-x-30'>
+                <div className='flex flex-col gap-y-5'>
+                    <div className='text-xl'>
+                        О нас
+                    </div>
+                    <div>
+                        <div className='text-lg text-[#ABABAB]'>
+                            <p>О продавце</p>
+                            <p>Партнёры</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex flex-col gap-y-5'>
+                    <div className='text-xl'>
+                        Покупателям
+                    </div>
+                    <div>
+                        <div className='text-lg text-[#ABABAB]'>
+                            <p>Правила</p>
+                            <p>Возврат</p>
+                            <p>Скидки</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className='flex flex-col gap-y-5'>
+                    <div className='text-xl'>
+                        Сотрудничество
+                    </div>
+                    <div>
+                        <div className='text-lg text-[#ABABAB]'>
+                            <p>Условия</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex flex-col gap-y-5'>
+                    <div className='text-xl'>
+                        Важно
+                    </div>
+                    <div>
+                        <div className='text-lg text-[#ABABAB]'>
+                            <p>Акции</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex flex-col gap-y-5'>
+                    <div className='text-xl'>
+                        Наши соц сети
+                    </div>
+                    <div>
+                        <div className='text-lg text-[#ABABAB]'>
+                            <p>ВКонтакте</p>
+                            <p>Одноклассники</p>
+                            <p>Телеграм</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex flex-col gap-y-5'>
+                    <div className='text-xl'>
+                        Контакты
+                    </div>
+                    <div>
+                        <div className='text-lg text-[#ABABAB]'>
+                            <p>8 800 000-00-01</p>
+                            <p>8 800 000-00-02</p>
+                            <p>bookshop@mail.ru</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Button from './Button'
 
 function Header() {
     return (
         <>
-            <div className='bg-gray-400 sticky top-0'>
-                <div className='text-xl  font-semibold'>Header:</div>
+            <div className='flex flex-row bg-[#B6B4A4] sticky top-0 py-2 px-4 justify-between'>
+                {/* <div className='text-xl  font-semibold'>Header:</div>
                 <div className='text-blue-600 space-x-2'>
                     <Link to='/'>Home</Link>
                     <Link to='/blog'>Blog</Link>
                     <Link to='/About'>About</Link>
+                </div> */}
+                <div className='flex flex-row gap-x-4'>
+                    <Button color="primary" size="medium" title="Главная"/>
+                    <Button color="primary" size="medium" title="Вход"/>
+                    <Button color="primary" size="medium" title="Помощь"/>
                 </div>
+                <div className='flex basis-1/2 items-center bg-white text-gray-400 text-xl px-2 rounded-md border-2 border-gray-400'>
+                    Поиск..
+                </div>
+               <Button color="primary" size="medium" title="Корзина"/>
             </div>
         </>
     )
